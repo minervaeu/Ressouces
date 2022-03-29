cfe1f8c3137368404caa0ea77f8109c655b2c6c3
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-03-29 18:53:35 +0200

    packages

--- linux/linux.js
+++ linux/linux.js
@@ -3,9 +3,11 @@ wkdvie@wkdvie-LIFEBOOK-E754:/$ sudo find -type f -name '*eruption*' -delete
 ____________________________
 https://www.liquidweb.com/kb/how-to-create-a-software-install-list/
 
+apt list --installed > allthethings.txt
 Install software from a text file:
 [root@host ~]# xargs -a allthethings.txt apt install
 
+dpkg-query -f '${binary:Package}\n' -W > allthethings.txt
 Install software from a text file:
 [root@host ~]# apt-get install < allthethings.txt
 
