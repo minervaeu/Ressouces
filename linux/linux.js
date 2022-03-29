bcd20828c066178a404c35437aeac6d9468bf94f
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-03-29 22:49:50 +0200

    up

--- linux/linux.js
+++ linux/linux.js
@@ -14,4 +14,8 @@ Install software from a text file:
 Further Detail
 After reinstalling your base Linux system, copy or upload a copy of the ‘allthethings.txt’ file to your system. Please ensure you have installed the same version of your OS on your new or remote system. Once the file has been copied, install the packages from the allthethings.txt file using one of the above noted commands as the root user. The package manager will install all of the packages listed in the ‘allthethings.txt’ file on your system.
 ________________
+hb.h
+sudo nano /usr/include/pango-1.0/pango/pango-coverage.h
+#include <hb.f>
+sudo nano pango-coverage.h
 
