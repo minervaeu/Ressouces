c68e5871bee21ad781a4f0be3fcbaa6614a75e3c
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-06-02 17:28:58 +0200

    .

---
+++ react/react-fast-setup/co38a4834950b81f65b4187b7/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import Overview from './components/Overview';
+
+export default function App() {
+  return (
+    <div className="App">
+        <Overview />
+    </div>
+  );
+};
---
+++ react/react-fast-setup/co38a4834950b81f65b4187b7/components/Overview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Overview.css';
+
+export default  function Overview() {
+  return (
+    <div className="Overview">
+        <h1>MongoDB || React Exercising-App</h1>
+    </div>
+  );
+};
+
+
