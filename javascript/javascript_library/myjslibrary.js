4f1ca4089674afecb9621527e72971970d80357c
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-06-02 19:05:24 +0200

    js library: 90deg abimation by DOM-Element

--- javascript/javascript_library/myjslibrary.js
+++ javascript/javascript_library/myjslibrary.js
@@ -28,10 +28,55 @@
 */
 //#endregion
 
-//#region  2) my generic-javascript library
+//#region my generic-javascript library
 
 /* ========================
-!    returns sum of givenm arguments
+!    toggle 90deg animation by element 
+========================== */
+function toggle90degAnimation (element) {
+
+    if(!element.classList.contains('add90degAni')){
+      element.classList.add('add90degAni');
+      element.classList.remove('remove90degAni');
+    } else {
+      element.classList.remove('add90degAni');
+      element.classList.add('remove90degAni');
+    };
+  
+// ? required and adaptable css-rules
+/* 
+.add90degAni{
+  animation: add90degAnimation .1s forwards 1 ease-in; 
+}
+
+.remove90degAni{
+  animation: remove90degAnimation .1s forwards 1 ease-in; 
+}
+
+@keyframes add90degAnimation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(90deg);
+  }
+}
+
+@keyframes remove90degAnimation {
+  0% {
+    transform: rotate(90deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
+*/
+
+};
+
+
+/* ========================
+!    returns sum of given arguments
 ========================== */
 function sum (a, ...rest){
     // Returns the sum of all given values
