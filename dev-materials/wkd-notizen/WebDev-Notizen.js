3debddacddde6d8a19d3fbc9a2adb1eab3c99fd7
Steve B <wkdatabase@gmail.com>
2022-08-01 09:03:42 +0200

    .

--- dev-materials/wkd-notizen/WebDev-Notizen.js
+++ dev-materials/wkd-notizen/WebDev-Notizen.js
@@ -160,7 +160,18 @@ git push
 git pull => Änderungen von guithub holen
 CONTINUS DEPLOYEMENT?
 
-
+…or create a new repository on the command line
+echo "# y" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/StefanBartl/y.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/StefanBartl/y.git
+git branch -M main
+git push -u origin main
 todo  ==== BRANCHES ==== 
  
 
